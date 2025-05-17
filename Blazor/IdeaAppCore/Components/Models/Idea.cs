896c79015fa2d3d5feda6e9e251f67dfc6b5459b
thymos77 <thymos77@gmail.com>
2025-05-17 15:34:57 +0900

    TEst

---
+++ Blazor/IdeaAppCore/Components/Models/Idea.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdeaAppCore.Components.Models
+{
+    public class Idea
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Note {  get; set; }
+
+        public interface IIdeaRepository
+        {
+            Task<Idea> AddIdea(Idea idea);
+            Task<List<Idea>> GetIdeas();
+
+        }
+
+        public class IdeaReporitory:IIdeaRepository
+        {
+            public Task<Idea> AddIdea(Idea idea)
+            {
+                throw new System.NotImplementedException();
+            }
+             
+            public Task<List<Idea>> GetIdeas()
+            {
+                throw new System.NotImplementedException();
+                
+            }
+        }
+    }
+}
